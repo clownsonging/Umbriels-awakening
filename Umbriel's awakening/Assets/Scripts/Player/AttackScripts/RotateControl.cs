a362d765b9208991ed54d7bec5bf57c7565fd817
Jamie Harmon <74502861+clownsonging@users.noreply.github.com>
2022-02-13 16:01:04 +0000

    Revert "Added room Generation"
    This reverts commit 0babf904f4c6ff8b721da2d678b078c2e4db3f28.

--- Umbriel's awakening/Assets/Scripts/Player/AttackScripts/RotateControl.cs
+++ Umbriel's awakening/Assets/Scripts/Player/AttackScripts/RotateControl.cs
@@ -28,46 +28,46 @@ public class RotateControl : MonoBehaviour
 
     private float Direction()
     {
-        Debug.Log(arrows.ReadValue<Vector2>());
+        //Debug.Log(arrows.ReadValue<Vector2>());
         Vector2 v2 = arrows.ReadValue<Vector2>();
         if (v2.x == 1)
         {
-            angle = 135;
+            angle = 90;
             return angle; 
         }
         if (v2.x == -1)
         {
-            angle = 315;
+            angle = 270;
             return angle;
         }
         if (v2.y == 1)
         {
-            angle = 45;
+            angle = 0;
             return angle;
         }
         if (v2.y == -1)
         {
-            angle = 225;
+            angle = 180;
             return angle;
         }
         if (v2.x > .1 && v2.y > .1)
         {
-            angle = 90;
+            angle = 45;
             return angle;
         }
         if (v2.x < -.1 && v2.y < -.1)
         {
-            angle = 270;
+            angle = 225;
             return angle;
         }
         if (v2.x > .1 && v2.y < -.1)
         {
-            angle = 180;
+            angle = 135;
             return angle;
         }
         if (v2.x < -.1 && v2.y > .1)
         {
-            angle = 0;
+            angle = 315;
             return angle;
         }
         else
--- Umbriel's awakening/Assets/Scripts/Terrain/RoomNavigation.cs
+++ Umbriel's awakening/Assets/Scripts/Terrain/RoomNavigation.cs
@@ -4,24 +4,11 @@ using UnityEngine;
 
 public class RoomNavigation : MonoBehaviour
 {
-    private Transform childHolder;
     [SerializeField] private GameObject North;
     [SerializeField] private GameObject South;
     [SerializeField] private GameObject East;
     [SerializeField] private GameObject West;
     // Start is called before the first frame update
-    void Start()
-    {
-    }
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    private void OnTriggerEnter(Collider other)
-    {
-
-    }
     public void Portals(bool north, bool east, bool south, bool west)
     {
         North.SetActive(north);
